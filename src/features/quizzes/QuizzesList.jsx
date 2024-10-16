83289d00bf752f28013317ae443d422a9b852d02
tojamrcel <marcelgala42@gmail.com>
2024-10-16 21:26:18 +0200

    feat: implement data to json file

--- src/features/quizzes/QuizzesList.jsx
+++ src/features/quizzes/QuizzesList.jsx
@@ -1,58 +1,6 @@
 import Button from "../../ui/Button"
 import QuizItem from "./QuizItem"
 
-const data = [
-    {
-        id: 1,
-        title: "Quiz about me",
-        author: "Marcel G",
-        description:
-            "In this quiz u can check how many things do you remember about me.",
-    },
-    {
-        id: 2,
-        title: "Quiz about you",
-        author: "Marcel Puta",
-        description:
-            "In this quiz u can check how many things do you remember about yourself.",
-    },
-    {
-        id: 2,
-        title: "Quiz about you",
-        author: "Marcel Puta",
-        description:
-            "In this quiz u can check how many things do you remember about yourself.",
-    },
-    {
-        id: 2,
-        title: "Quiz about you",
-        author: "Marcel Puta",
-        description:
-            "In this quiz u can check how many things do you remember about yourself.",
-    },
-    {
-        id: 2,
-        title: "Quiz about you",
-        author: "Marcel Puta",
-        description:
-            "In this quiz u can check how many things do you remember about yourself.",
-    },
-    {
-        id: 2,
-        title: "Quiz about you",
-        author: "Marcel Puta",
-        description:
-            "In this quiz u can check how many things do you remember about yourself.",
-    },
-    {
-        id: 2,
-        title: "Quiz about you",
-        author: "Marcel Puta",
-        description:
-            "In this quiz u can check how many things do you remember about yourself.",
-    },
-]
-
 function QuizzesList() {
     return (
         <div className="flex flex-col items-center">
