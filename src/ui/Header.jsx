65063b94f38c5b30c9ff6e200eb1ed5098c3045c
tojamrcel <marcelgala42@gmail.com>
2024-10-16 20:34:26 +0200

    style: add icon to QuizApp logo

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,10 @@
+import { FaReact } from "react-icons/fa"
+
 function Header() {
     return (
         <header className="w-full py-4">
-            <h1 className="text-center text-6xl font-bold text-gray-300">
+            <h1 className="flex justify-center gap-3 text-center text-6xl font-bold text-gray-300">
+                <FaReact style={{ color: "#581c87" }} />
                 Quiz App
             </h1>
         </header>
