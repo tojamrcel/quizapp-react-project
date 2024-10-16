9be964cbaeb3a9fbcb6f6e90918eecc2e530795e
tojamrcel <marcelgala42@gmail.com>
2024-10-16 20:41:25 +0200

    refactor: create QuizItem component to make code more readable

--- src/features/quizzes/QuizzesList.jsx
+++ src/features/quizzes/QuizzesList.jsx
@@ -1,3 +1,5 @@
+import QuizItem from "./QuizItem"
+
 const data = [
     {
         id: 1,
@@ -54,18 +56,7 @@ function QuizzesList() {
     return (
         <ul className="no-scrollbar flex max-h-[90dvh] flex-col gap-8 overflow-auto scroll-smooth px-4 py-8 shadow-sm">
             {data.map((quiz) => (
-                <li
-                    className="hover:scale flex max-h-[12rem] max-w-full cursor-default flex-col items-start justify-center gap-1 rounded-md border bg-gray-300 p-8 transition-all duration-500 hover:translate-y-[-4px]"
-                    key={quiz.id}
-                >
-                    <h2 className="flex items-center text-2xl font-bold tracking-tighter text-zinc-800">
-                        {quiz.title}
-                    </h2>
-                    <p className="text-gray-600">{quiz.description}</p>
-                    <button className="my-2 rounded-md bg-violet-900 px-4 py-2 font-bold uppercase tracking-tight text-gray-200 transition-all duration-300 hover:cursor-pointer hover:bg-violet-950">
-                        Play
-                    </button>
-                </li>
+                <QuizItem quiz={quiz} key={quiz.id} />
             ))}
         </ul>
     )
