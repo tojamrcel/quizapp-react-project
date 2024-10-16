86bf6999a0a2313cde1f0c68a8699204d88a8a26
tojamrcel <marcelgala42@gmail.com>
2024-10-16 19:05:07 +0200

    feat: build App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import AppLayout from "./ui/AppLayout"
+import Quizzes from "./pages/quizzes"
+import Quiz from "./pages/Quiz"
 
 function App() {
     return (
-        <div>
-            <AppLayout />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AppLayout />}>
+                    <Route
+                        index
+                        element={<Navigate replace to={"quizzes"} />}
+                    ></Route>
+                    <Route path="quizzes" element={<Quizzes />}></Route>
+                    <Route path="quiz/:quizId" element={<Quiz />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
