1e9356430c5b74562699f056282b04ea2ac40744
tojamrcel <marcelgala42@gmail.com>
2024-10-18 17:46:34 +0200

    feat: implement timer after answer

--- src/features/quiz/ActiveQuiz.jsx
+++ src/features/quiz/ActiveQuiz.jsx
@@ -1,8 +1,10 @@
 import { useQuizzes } from "../../contexts/QuizzesContext"
 import AnswerItem from "./AnswerItem"
 import Spinner from "../../ui/Spinner"
+import { useState } from "react"
 
 function ActiveQuiz() {
+    const [seconds, setSeconds] = useState(3)
     const { activeQuiz, dispatch } = useQuizzes()
     const { currentQuestion, questions, answer } = activeQuiz
     const question = questions?.at(currentQuestion)
@@ -12,37 +14,50 @@ function ActiveQuiz() {
     function handleClick(userAnswerIndex) {
         if (answer !== null) return
         dispatch({ type: "newAnswer", payload: userAnswerIndex })
+        setSeconds(3)
+
+        const timer = setInterval(function () {
+            setSeconds((seconds) => seconds - 1)
+        }, 1000)
 
         setTimeout(function () {
             dispatch({
                 type: "nextQuestion",
                 payload: questions?.at(currentQuestion + 1).correctAnswer,
             })
-        }, 2000)
+            clearInterval(timer)
+        }, 3000)
     }
 
     return (
-        <div className="flex min-h-[60dvh] w-full items-center justify-center">
-            <div className="flex w-[90%] flex-col justify-center gap-5">
-                <h2 className="text-3xl font-bold text-gray-200 md:text-4xl">
-                    <span className="text-violet-900">
-                        {currentQuestion + 1}.
-                    </span>{" "}
-                    {question.question}
-                </h2>
-                <ul className="flex flex-col gap-3 text-lg">
-                    {question.answers.map((ans, i) => (
-                        <AnswerItem
-                            index={i}
-                            answer={ans}
-                            key={ans}
-                            onClick={handleClick}
-                            active={i === activeQuiz.answer}
-                        />
-                    ))}
-                </ul>
+        <>
+            <div className="flex min-h-[60dvh] w-full flex-col items-center justify-center">
+                <div className="flex w-[90%] flex-col justify-center gap-5">
+                    <h2 className="text-3xl font-bold text-gray-200 md:text-4xl">
+                        <span className="text-violet-900">
+                            {currentQuestion + 1}.
+                        </span>{" "}
+                        {question.question}
+                    </h2>
+                    <ul className="flex flex-col gap-3 text-lg">
+                        {question.answers.map((ans, i) => (
+                            <AnswerItem
+                                index={i}
+                                answer={ans}
+                                key={ans}
+                                onClick={handleClick}
+                                active={i === activeQuiz.answer}
+                            />
+                        ))}
+                    </ul>
+                </div>
+                {answer !== null ? (
+                    <span className="text-zinc-500">
+                        Next question in {seconds}...
+                    </span>
+                ) : null}
             </div>
-        </div>
+        </>
     )
 }
 
