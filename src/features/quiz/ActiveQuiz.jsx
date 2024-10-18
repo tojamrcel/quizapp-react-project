01c417f6afd0b3e23ae20914f9ea5b3cf1a1eb2b
tojamrcel <marcelgala42@gmail.com>
2024-10-18 16:10:15 +0200

    feat: implement displaying real data in ActiveQuiz

--- src/features/quiz/ActiveQuiz.jsx
+++ src/features/quiz/ActiveQuiz.jsx
@@ -1,17 +1,27 @@
+import { useQuizzes } from "../../contexts/QuizzesContext"
 import AnswerItem from "./AnswerItem"
+import Spinner from "../../ui/Spinner"
 
 function ActiveQuiz() {
+    const { activeQuiz } = useQuizzes()
+    const { currentQuestion, questions } = activeQuiz
+    const question = questions?.at(currentQuestion)
+
+    if (!activeQuiz.id) return <Spinner />
+
     return (
         <div className="flex min-h-[60dvh] w-full items-center justify-center">
             <div className="flex w-[90%] flex-col justify-center gap-5">
-                <h2 className="text-4xl font-bold text-gray-200">
-                    <span className="text-violet-900">1.</span> Question 1
+                <h2 className="text-3xl font-bold text-gray-200 md:text-4xl">
+                    <span className="text-violet-900">
+                        {currentQuestion + 1}.
+                    </span>{" "}
+                    {question.question}
                 </h2>
                 <ul className="flex flex-col gap-3 text-lg">
-                    <AnswerItem answer="Answer 1" />
-                    <AnswerItem answer="Answer 2" />
-                    <AnswerItem answer="Answer 3" />
-                    <AnswerItem answer="Answer 4" />
+                    {question.answers.map((ans) => (
+                        <AnswerItem answer={ans} key={ans} />
+                    ))}
                 </ul>
             </div>
         </div>
