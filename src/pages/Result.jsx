d4038eec545ee79fd2c07e2e9fd3d7e7028e43f7
tojamrcel <marcelgala42@gmail.com>
2024-10-19 15:14:35 +0200

    feat, style: create result page, change styles for Button component

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,5 +1,17 @@
+import Button from "../ui/Button"
+
 function Result() {
-    return <div>Result: 5/10</div>
+    return (
+        <div className="container mx-auto my-16 max-w-[80%] md:my-32">
+            <div className="flex flex-col items-center justify-center gap-6 text-center font-bold text-zinc-300">
+                <h2 className="text-6xl md:text-8xl">Your result is</h2>
+                <span className="text-7xl font-semibold text-violet-900">
+                    5<span className="text-gray-300"> / 10</span>
+                </span>
+                <Button type="wide">PLAY SOME OTHER QUIZZES</Button>
+            </div>
+        </div>
+    )
 }
 
 export default Result
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,19 @@
 function Button({ onClick, type, children }) {
+    if (type === "wide")
+        return (
+            <button
+                onClick={() => onClick?.()}
+                className="my-4 h-12 w-[80%] cursor-pointer rounded-md bg-violet-900 px-2 font-bold text-gray-200 transition-all duration-300 hover:bg-violet-950 md:w-[60%]"
+            >
+                {children}
+            </button>
+        )
+
     if (type === "full-width")
         return (
             <button
                 onClick={() => onClick?.()}
-                className="my-4 h-12 w-[80%] cursor-pointer rounded-md bg-violet-900 font-bold text-gray-200 transition-all duration-300 hover:bg-violet-950 md:w-full"
+                className="my-4 h-12 w-[80%] cursor-pointer rounded-md bg-violet-900 px-2 font-bold text-gray-200 transition-all duration-300 hover:bg-violet-950 md:w-full"
             >
                 {children}
             </button>
