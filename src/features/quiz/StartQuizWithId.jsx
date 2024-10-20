cbb56fcc6a08cc162ff042ce4c3b4420b70be000
tojamrcel <marcelgala42@gmail.com>
2024-10-20 22:44:35 +0200

    style: change border radius on startquizwithid component

--- src/features/quiz/StartQuizWithId.jsx
+++ src/features/quiz/StartQuizWithId.jsx
@@ -7,7 +7,7 @@ function StartQuizWithId({ quiz }) {
 
     return (
         <div className="my-6 flex min-h-[60dvh] items-center justify-center">
-            <div className="flex max-w-[80%] flex-col items-center justify-center gap-4 rounded-md bg-zinc-300 px-[1rem] py-[3rem] shadow-lg md:px-[6rem] md:py-[6rem]">
+            <div className="flex max-w-[80%] flex-col items-center justify-center gap-4 rounded-xl bg-zinc-300 px-[1rem] py-[3rem] shadow-lg md:px-[6rem] md:py-[6rem]">
                 <h2 className="text-center text-3xl font-bold leading-9 md:text-4xl">
                     Would you like to start the{" "}
                     <span className="md:block">"{quiz.title}"</span> quiz?
