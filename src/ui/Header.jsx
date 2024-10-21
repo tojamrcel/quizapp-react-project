72fb7b45f6a25a822f2d9bed69f8cb249c69ee73
tojamrcel <marcelgala42@gmail.com>
2024-10-21 21:23:47 +0200

    style: add some styles to make page looks better on smaller devices

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,8 +2,8 @@ import { FaReact } from "react-icons/fa"
 
 function Header() {
     return (
-        <header className="w-full py-4">
-            <h1 className="flex justify-center gap-3 text-center text-6xl font-bold text-gray-300">
+        <header className="w-full py-2 sm:py-4">
+            <h1 className="flex justify-center gap-2 text-center text-6xl font-bold text-gray-300 sm:gap-3">
                 <FaReact style={{ color: "#581c87" }} />
                 Quiz App
             </h1>
