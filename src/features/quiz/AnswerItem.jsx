7870adaf7a7f0c7335a2d6a3890483b6ab8e60da
tojamrcel <marcelgala42@gmail.com>
2024-10-21 22:27:25 +0200

    style: apply some styles related to disabled state in AnswerItem component

--- src/features/quiz/AnswerItem.jsx
+++ src/features/quiz/AnswerItem.jsx
@@ -1,8 +1,8 @@
 import { useQuizzes } from "../../contexts/QuizzesContext"
 
-function AnswerItem({ index, answer, onClick, active }) {
+function AnswerItem({ index, answer, onClick, active, disabled }) {
     const { activeQuiz } = useQuizzes()
-    const { correctAnswer } = activeQuiz
+    const { correctAnswer, answer: answered } = activeQuiz
     const classStr =
         index === correctAnswer
             ? "border-4 border-green-700"
@@ -10,8 +10,8 @@ function AnswerItem({ index, answer, onClick, active }) {
 
     return (
         <li
-            onClick={() => onClick(index)}
-            className={`${active ? classStr : "border-transparent"} transition-color cursor-pointer rounded-sm border-4 bg-gray-300 px-4 py-3 shadow-md duration-200 hover:scale-[1.01]`}
+            onClick={() => (!disabled ? onClick(index) : null)}
+            className={`${active ? classStr : "border-transparent"} ${disabled ? "cursor-default opacity-85" : "hover:scale-[1.01]"} ${answered !== null || disabled ? "cursor-default" : "cursor-pointer"} transition-color rounded-sm border-4 bg-gray-300 px-4 py-3 shadow-md duration-200`}
         >
             <p className="font-bold text-zinc-900">{answer}</p>
         </li>
