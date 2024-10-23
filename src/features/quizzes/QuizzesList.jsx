f15c0653f8f6dee15974614b550d4d8744e7776f
tojamrcel <marcelgala42@gmail.com>
2024-10-23 17:46:19 +0200

    feat: implement modal

--- src/features/quizzes/QuizzesList.jsx
+++ src/features/quizzes/QuizzesList.jsx
@@ -3,6 +3,8 @@ import Button from "../../ui/Button"
 import QuizItem from "./QuizItem"
 import Spinner from "../../ui/Spinner"
 import Menus from "../../ui/Menus"
+import Modal from "../../ui/Modal"
+import CreateQuizForm from "../quiz/CreateQuizForm"
 
 function QuizzesList() {
     const { quizzes, status } = useQuizzes()
@@ -15,23 +17,30 @@ function QuizzesList() {
         )
 
     return (
-        <Menus>
-            <div className="flex flex-col items-center">
-                <ul className="no-scrollbar flex max-h-[75dvh] w-full flex-col gap-8 overflow-auto scroll-smooth px-4 py-8 shadow-sm md:max-h-[85dvh]">
-                    {quizzes.length ? (
-                        quizzes.map((quiz) => (
-                            <QuizItem quiz={quiz} key={quiz.id} />
-                        ))
-                    ) : (
-                        <p className="text-center text-xl font-bold text-gray-200">
-                            There are no quizzes at the moment, but you can
-                            create your own quiz!
-                        </p>
-                    )}
-                </ul>
-                <Button type="full-width">CREATE YOUR OWN QUIZ</Button>
-            </div>
-        </Menus>
+        <Modal>
+            <Menus>
+                <div className="flex flex-col items-center">
+                    <ul className="no-scrollbar flex max-h-[75dvh] w-full flex-col gap-8 overflow-auto scroll-smooth px-4 py-8 shadow-sm md:max-h-[85dvh]">
+                        {quizzes.length ? (
+                            quizzes.map((quiz) => (
+                                <QuizItem quiz={quiz} key={quiz.id} />
+                            ))
+                        ) : (
+                            <p className="text-center text-xl font-bold text-gray-200">
+                                There are no quizzes at the moment, but you can
+                                create your own quiz!
+                            </p>
+                        )}
+                    </ul>
+                    <Modal.Window name="create-quiz">
+                        <CreateQuizForm />
+                    </Modal.Window>
+                    <Modal.Open opens="create-quiz">
+                        <Button type="full-width">CREATE YOUR OWN QUIZ</Button>
+                    </Modal.Open>
+                </div>
+            </Menus>
+        </Modal>
     )
 }
 
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,4 +1,6 @@
 import { cloneElement, createContext, useContext, useState } from "react"
+import { createPortal } from "react-dom"
+import { HiXMark } from "react-icons/hi2"
 
 const ModalContext = createContext()
 
@@ -20,8 +22,25 @@ function Open({ children, opens: opensWindowName }) {
     return cloneElement(children, { onClick: () => open(opensWindowName) })
 }
 
-function Window({ children }) {
-    return children
+function Window({ children, name }) {
+    const { openName, close } = useContext(ModalContext)
+
+    if (openName !== name) return null
+
+    return createPortal(
+        <div className="fixed bottom-0 top-0 h-[100dvh] w-full backdrop-blur-md">
+            <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-300 px-[3.2rem] py-[4rem] shadow-md">
+                <button
+                    onClick={close}
+                    className="absolute right-2 top-2 text-2xl"
+                >
+                    <HiXMark />
+                </button>
+                <div>{children}</div>
+            </div>
+        </div>,
+        document.body,
+    )
 }
 
 Modal.Open = Open
