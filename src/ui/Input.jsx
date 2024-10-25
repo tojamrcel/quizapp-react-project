6cd69fdd168d70ac911757f9b195f55c75c81078
tojamrcel <marcelgala42@gmail.com>
2024-10-25 19:54:13 +0200

    fix: change autocomplete in input to off

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -2,6 +2,7 @@ function Input({ variation, register }) {
     if (variation === "sm")
         return (
             <input
+                autoComplete="off"
                 type="text"
                 {...register}
                 className="h-8 max-w-[70%] rounded-sm border-violet-800 bg-gray-100 p-2 outline-none focus:border-2"
@@ -11,6 +12,7 @@ function Input({ variation, register }) {
     if (variation === "normal" || !variation)
         return (
             <input
+                autoComplete="off"
                 className="h-8 min-w-[50dvw] rounded-sm border-violet-800 bg-gray-100 p-2 outline-none focus:border-2"
                 type="text"
                 {...register}
