fec2fbc92c21c550a93a64b8ca3271125b505bd3
tojamrcel <marcelgala42@gmail.com>
2024-10-30 21:31:39 +0100

    style: change styles for confirmDelete

--- src/ui/ConfirmDelete.jsx
+++ src/ui/ConfirmDelete.jsx
@@ -1,6 +1,6 @@
 function ConfirmDelete({ onCloseModal, onConfirm }) {
     return (
-        <div className="flex max-w-[40rem] flex-col gap-[1.2rem]">
+        <div className="flex max-h-[15rem] max-w-[40rem] flex-col gap-[1.2rem]">
             <h3 className="text-3xl font-bold">Delete Quiz</h3>
             <p className="mb-[1.2rem] text-lg text-zinc-800">
                 Are you sure you want to delete this quiz permamently? This
@@ -18,7 +18,7 @@ function ConfirmDelete({ onCloseModal, onConfirm }) {
                         onConfirm?.()
                         onCloseModal()
                     }}
-                    className="transition-color text-md rounded-md bg-red-700 px-[1.6rem] py-[1rem] font-bold text-gray-300 duration-200 hover:bg-red-800"
+                    className="transition-color text-md rounded-md bg-red-700 px-[1.6rem] py-[0.8rem] font-bold text-gray-300 duration-200 hover:bg-red-800"
                 >
                     Delete
                 </button>
