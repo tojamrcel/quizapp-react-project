69ce4779c73eadc647325b376be702e7b9b8f73d
tojamrcel <marcelgala42@gmail.com>
2024-10-30 21:58:43 +0100

    feat: add message for demo version of app

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -2,9 +2,17 @@ import QuizzesList from "../features/quizzes/QuizzesList"
 
 function Quizzes() {
     return (
-        <div className="mx-auto block max-h-[100rem] max-w-[50rem]">
-            <QuizzesList />
-        </div>
+        <>
+            {/* <div className="flex min-h-16 items-center justify-center bg-violet-800">
+                <p className="text-gray-300">
+                    👋 FOR DEMO VERSION OF APPLICATION ALL API FEATURES HAVE
+                    BEEN IMPLEMENTED LOCALLY
+                </p>
+            </div> */}
+            <div className="mx-auto block max-h-[100rem] max-w-[50rem]">
+                <QuizzesList />
+            </div>
+        </>
     )
 }
 
