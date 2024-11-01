62e9e0a421314f28a6a0df45df56bc282352b035
tojamrcel <marcelgala42@gmail.com>
2024-11-01 10:09:04 +0100

    refactor: shorten demo app message

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -3,12 +3,12 @@ import QuizzesList from "../features/quizzes/QuizzesList"
 function Quizzes() {
     return (
         <>
-            {/* <div className="flex min-h-16 items-center justify-center bg-violet-800">
-                <p className="text-gray-300">
-                    👋 FOR DEMO VERSION OF APPLICATION ALL API FEATURES HAVE
-                    BEEN IMPLEMENTED LOCALLY
+            <div className="flex min-h-16 items-center justify-center bg-violet-800">
+                <p className="text-center text-gray-300">
+                    👋 FOR DEMO VERSION OF APP ALL API FEATURES HAVE BEEN
+                    IMPLEMENTED LOCALLY
                 </p>
-            </div> */}
+            </div>
             <div className="mx-auto block max-h-[100rem] max-w-[50rem]">
                 <QuizzesList />
             </div>
